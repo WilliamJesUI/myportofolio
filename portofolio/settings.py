4b0888720355da70140ff5735615bd496527d613
WilliamJesiel <william.jesiel@ui.ac.id>
2026-09-02 15:27:48 +0700

    Fixed allowed host typo

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "william.jesiel-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "william-jesiel-myportofolio.pws.cs.ui.ac.id"]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
