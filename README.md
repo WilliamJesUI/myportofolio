6de48ed6f68b414f1d80e27cb9d4b338763343c6
WilliamJesiel <william.jesiel@ui.ac.id>
2026-09-14 19:35:13 +0700

    Updated unit test + reflection        Updated the unit test that tests for the project, achievement, and experience page. Now it tests the new models. Also updated the README.md so now it includes the Reflection

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ Class : PBP KKI
 
 "DID not use AI" 
 I mainly studied from the W3school website! I found their premade templates really helpful. My work process mainly revolves around using their generic template and reformatting it so it looks nice on my page. I also refer to some sections from tutorial 1, especially with the flexbox styling. I change somethings a little mainly through trial and error until I get the desired look!
+
+
+### Assignment 2
+1. From the user point of view, they would notice only the new 'achievement' page. In reality, I actually refactored the main page by changing the originally hardcoded project flipcards into for loops. Although it makes zero difference for the viewers, it makes a huge difference for me as the developer due to reasons I will explain in number 2. Also Idk why in the PWS I have to retype the shell code to insert my projects and experience. Its so frustrating everytime I push to PWS all my projects, experience and achievement is gone. >:(
+2. For me, I think storing the data in the model actually made my html file way shorter since I can now iterate through all the data with a for loop. As a result, this made my code more readable. Not only that, I can now easily modify which data I want to display. Example using If and else, I could show perhaps the projects that has 'burhan' in it. If it were to be hard coded I would have to manually select which projects to remove and it will give me a headache.
+3. The 'makemigrations' command will create some python files that will either create or modify the database. Inside the python file lies the instruction that tell the database what needs to be done. Then after 'makemigrations' there is the 'migrate'. 'migrate' will actually apply the changes to the database, updating its schema accordingly.
+
+"DID not use AI"
+
+I still have not used AI in the making of this assignment. I felt like this part is more straightforward because I've watched a free lecture from CS50W Harvard in youtube that discusses about the MVT. The explanation was amazing and I was able to immediately implement it by referring to their notes online. Of course I also found the tutorial 2 helpful, I followed the steps there to create my new models 'Project' and 'Achievement'. Unit tests weren't that bad aswell because I previously learned it in DDP2. 
+
+Low-key I don't know how much longer I can survive without AI. I want to make my website beautiful but it seems so impossible without AI because I don't have much time :( CSS is so hard.
--- main/tests.py
+++ main/tests.py
@@ -2,29 +2,62 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Achievement, Experience, Project
 
 
 class MainTest(TestCase):
     def setUp(self):
+        self.achievement = Achievement.objects.create(
+            name="Hackathon",
+            description="Created Chatgpt myself!",
+            thumbnail="image.png",
+        )
+
         self.experience = Experience.objects.create(
             title="IDS Teaching Assistant",
             description="Help students understand digital systems.",
             category="part-time",
         )
 
+        self.project = Project.objects.create(
+            name="Burhan Quest",
+            description="I love Burhan",
+            thumbnail="Burhan.png",
+            download_url="www.burhan.com",
+
+        )
+
     def test_main_url_is_accessible(self):
-        response = self.client.get(reverse("main:show_main"))
+        response=self.client.get(reverse("main:show_main"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
+        self.assertNotContains(response, self.achievement.name)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_achievement")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/a-page-that-does-not-exist/")
 
         self.assertEqual(response.status_code, 404)
+
+    def test_project_model(self):
+        self.assertEqual(str(self.project), "Burhan Quest")
+        self.assertEqual(self.project.description, "I love Burhan")
+        self.assertEqual(self.project.thumbnail, "Burhan.png")
+        self.assertEqual(self.project.download_url, "www.burhan.com")
+
+    def test_project_page(self):
+        response = self.client.get(reverse("main:show_main"))
+        self.assertContains(response, self.project.name)
+        self.assertContains(response, self.project.description)
+
+    def test_empty_project_page(self):
+        Project.objects.all().delete()
+        response = self.client.get(reverse("main:show_main"))
+
+        self.assertContains(response, "No Project has been added yet!")
 
     def test_experience_model(self):
         self.assertEqual(str(self.experience), "IDS Teaching Assistant")
@@ -42,6 +75,7 @@ class MainTest(TestCase):
         self.assertContains(response, "Ongoing")
         self.assertContains(response, f'href="{reverse("main:show_main")}"')
 
+
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
@@ -56,3 +90,26 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Completed")
         self.assertNotContains(response, "Ongoing")
+
+    def test_achievement_model(self):
+        self.assertEqual(str(self.achievement), "Hackathon")
+        self.assertEqual(self.achievement.name, "Hackathon")
+        self.assertEqual(self.achievement.description, "Created Chatgpt myself!")
+        self.assertEqual(self.achievement.thumbnail, "image.png")
+
+    def test_achievement_page(self):
+        response = self.client.get(reverse("main:show_achievement"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "achievement.html")
+        self.assertContains(response, self.achievement.name)
+        self.assertContains(response, self.achievement.description)
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+
+    def test_empty_achievement_page(self):
+        Achievement.objects.all().delete()
+        response = self.client.get(reverse("main:show_achievement"))
+
+        self.assertContains(response, "William is kinda achievementless rn...")
+
